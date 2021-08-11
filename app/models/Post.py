4de0be74668a14a19805b4958bd430c7548114b0
Daniel Lewis <daniel.lewis@protonmail.com>
2021-08-11 12:36:30 -0600

    added Votes model

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,9 +1,8 @@
 from datetime import datetime
-
-from sqlalchemy.sql.expression import null
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relation, relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 
 class Post(Base):
@@ -18,3 +17,8 @@ class Post(Base):
     user = relationship('User')
 
     comments = relationship('Comment', cascade='all, delete')
+    votes = relationship('Vote', cascade='all, delete')
+
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
